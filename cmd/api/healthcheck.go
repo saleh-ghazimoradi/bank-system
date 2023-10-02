d80f05eb103468e31d1ad51cace1a298935b8ec7
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-10-02 10:54:34 +0330

    improve healthcheck error returnings

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -16,7 +16,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "the server encountered an error and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
