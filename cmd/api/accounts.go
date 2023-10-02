56c8bfbd3bd2c2697f0cecfabc1175120df723cb
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-10-02 10:28:51 +0330

    improve the showaccounthandler function

--- cmd/api/accounts.go
+++ cmd/api/accounts.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/saleh-ghazimoradi/bank-system.git/internal/data"
 )
 
 func (app *application) showAListOfAccountsHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +14,30 @@ func (app *application) showAListOfAccountsHandler(w http.ResponseWriter, r *htt
 }
 
 func (app *application) showAnAccountHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "show an account by ID")
+
+	id, err := app.readIDParam(r)
+
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	account := data.Account{
+		ID:        id,
+		CreatedAt: time.Now(),
+		FirstName: "Saleh",
+		LastName:  "Ghazimoradi",
+		Number:    int64(rand.Intn(9999999999999999)),
+		Balance:   2500,
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, account, nil)
+
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
 
 func (app *application) createAnAccountHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +50,4 @@ func (app *application) deleteAnAccountHandler(w http.ResponseWriter, r *http.Re
 
 func (app *application) updateAnAccountHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "update an account")
-}
-
-func (app *application) transferMoneyToAnAccountHandler(w http.ResponseWriter, r *http.ResponseWriter) {
-	fmt.Fprintln(w, "transfer money to an account")
 }
