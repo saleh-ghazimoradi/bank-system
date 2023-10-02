3c1f17658e394037f24a3c23837c07e2ea010554
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-10-02 10:30:15 +0330

    remove .gitkeep file from the directories

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,6 +11,13 @@ func (app *application) routes() *httprouter.Router {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	
+	router.HandlerFunc(http.MethodGet, "/v1/accounts", app.showAListOfAccountsHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/accounts/:id", app.showAnAccountHandler)
+
+	router.HandlerFunc(http.MethodPatch, "/v1/accounts/:id", app.updateAnAccountHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/accounts", app.createAnAccountHandler)
+
 	return router
 }
