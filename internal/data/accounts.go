d58fef20350af9575690f8f509e73723278e701b
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-10-02 18:03:28 +0330

    complete get and insert queries

--- internal/data/accounts.go
+++ internal/data/accounts.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/saleh-ghazimoradi/bank-system.git/internal/validator"
@@ -12,7 +13,7 @@ type Account struct {
 	FirstName string    `json:"firstName"`
 	LastName  string    `json:"lastName"`
 	Number    int64     `json:"number"`
-	Balance   float64   `json:"balance"`
+	Balance   int64     `json:"balance"`
 	CreatedAt time.Time `json:"createdAt"`
 	Version   int32     `json:"version"`
 }
@@ -31,11 +32,46 @@ type AccountModel struct {
 }
 
 func (a AccountModel) Insert(account *Account) error {
-	return nil
+	query := `
+        INSERT INTO bank (first_name, last_name, number, balance) 
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, created_at, version`
+
+	args := []any{account.FirstName, account.LastName, account.Number, account.Balance}
+	return a.DB.QueryRow(query, args...).Scan(&account.ID, &account.CreatedAt, &account.Version)
 }
 
 func (a AccountModel) Get(id int64) (*Account, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `SELECT id, created_at, first_name, last_name, number,balance,version
+	FROM bank
+	WHERE id = $1`
+
+	var account Account
+
+	err := a.DB.QueryRow(query, id).Scan(
+		&account.ID,
+		&account.CreatedAt,
+		&account.FirstName,
+		&account.LastName,
+		&account.Balance,
+		&account.Number,
+		&account.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &account, nil
+
 }
 
 func (a AccountModel) Update(account *Account) error {
